8dc2a4cc2bf3c7aa90d37a7fc40cbc49001453b4
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-07-06 00:03:48 +0100

    refactor: improve code structure and readability in estate_property model

--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -4,11 +4,11 @@ class EstateProperty(models.Model):
     _name="estate_property"
     _description= 'This is module to test my work'
     
-    name=fields.Char()
+    name=fields.Char(required=True)
     description=fields.Text()
     postcode=fields.Char()
     date_availability=fields.Date()
-    expected_price=fields.Float()
+    expected_price=fields.Float(required=True)
     selling_price=fields.Float()
     bedrooms=fields.Integer()
     living_area=fields.Integer()
