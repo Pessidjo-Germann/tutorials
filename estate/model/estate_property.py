74e626b9619a3ba9191e5d4d80e994a0c45150de
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-07-05 23:54:33 +0100

    fix select

--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -17,6 +17,6 @@ class EstateProperty(models.Model):
     garden=fields.Boolean('garden',default=True)
     garden_area=fields.Integer()
     garden_orientation=fields.Selection(
-        'garden',
+        string='garden',
         selection=[('North','South'),('East','West')]
     )
