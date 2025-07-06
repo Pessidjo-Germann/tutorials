83a299f88035ed4a21881b963fb436a0434fced0
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-07-06 15:25:41 +0100

    fix: update estate_property model fields for date_availability, selling_price, bedrooms, garden_area, and active

--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -1,5 +1,5 @@
 from odoo import fields,models
-
+from dateutil.relativedelta import relativedelta
 class EstateProperty(models.Model):
     _name="estate.property"
     _description= 'This is module to test my work'
@@ -7,15 +7,16 @@ class EstateProperty(models.Model):
     name=fields.Char(required=True)
     description=fields.Text()
     postcode=fields.Char()
-    date_availability=fields.Date()
+    date_availability=fields.Date(copy=False,default=lambda self: date.today() + relativedelta(months=3))
     expected_price=fields.Float(required=True)
-    selling_price=fields.Float()
-    bedrooms=fields.Integer()
+    selling_price=fields.Float(readonly=True,copy=False)
+    bedrooms=fields.Integer(default=2)
     living_area=fields.Integer()
     facades=fields.Integer()
     garage=fields.Boolean()
     garden=fields.Boolean('garden',default=True)
-    garden_area=fields.Integer()
+    garden_area=fields.Integer(),
+    active=fields.Boolean(),
     garden_orientation=fields.Selection(
         string='garden orientation',
         selection=[('North','South'),('East','West')]
