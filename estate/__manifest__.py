3c5adfeb64f8712ff579db43339a3d4bc913ba8d
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-07-06 06:27:38 +0100

    add views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,8 @@
 'version':'1.0',
 'depends':['base'],
 'data':[
-    'security/ir.model.access.csv'
+    'security/ir.model.access.csv',
+    'views/estate_property_views.xml'
     ],
 'application':True
 }
