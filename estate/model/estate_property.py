f0d649d161bda9b15bb0834c66855f2b766027f4
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-07-13 13:25:08 +0100

    fixo

--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -39,9 +39,9 @@ class EstateProperty(models.Model):
     default='new'
 )
     offer_ids=fields.One2many("estate.property.offer","property_id",string='Offer')
-    total_area=fields.Float("_area_total")
+    total_area=fields.Float(compute="_area_total")
     
-    @api.depends("amount")
+    @api.depends("garden_area")
     def _area_total(self):
         for record in self:
             record.total_area=record.garden_area+record.living_area
